105323c0d456286bde5827066dff689ac098d296
Aurelien <aurelien.badre.dev@gmail.com>
2020-05-07 12:16:50 +0200

    Added Quiz Link

--- list-games/src/components/GameList/GameList.js
+++ list-games/src/components/GameList/GameList.js
@@ -14,7 +14,7 @@ const GameList = () => {
   }, []);
 
   const deleteGame = (game) => {
-    //on prend en paramètre un film "game"
+    //on prend en paramètre un film "game" au singulier
     const position = games.indexOf(game); // Identifier la position de chaque jeu dans le tableau
     console.log(position); // au click sur les boutons chaque film affiche sa position
     games.splice(position, 1); // Elle prend en premier argument la position à partir de laquelle on commence la suppression et en deuxième argument le nombre d'éléments à supprimer.
