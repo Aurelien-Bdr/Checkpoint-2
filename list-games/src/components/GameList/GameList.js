a70b8f28c30b9a0e876cc3949ec60de5b394128e
Aurelien <aurelien.badre.dev@gmail.com>
2020-05-07 11:26:11 +0200

    Added a condition on the button filter

--- list-games/src/components/GameList/GameList.js
+++ list-games/src/components/GameList/GameList.js
@@ -5,6 +5,7 @@ import Game from "../Game/Game";
 
 const GameList = () => {
   const [games, setGames] = useState([]);
+  const [bestGames, setBestGames] = useState(false);
 
   useEffect(() => {
     axios
@@ -15,18 +16,27 @@ const GameList = () => {
   const deleteGame = (game) => {
     //on prend en paramètre un film "game"
     const position = games.indexOf(game); // Identifier la position de chaque jeu dans le tableau
-    console.log(position);
+    console.log(position); // au click sur les boutons chaque film affiche sa position
     games.splice(position, 1); // Elle prend en premier argument la position à partir de laquelle on commence la suppression et en deuxième argument le nombre d'éléments à supprimer.
     setGames([...games]);
     console.log(games); // L'element est bien supprimé du tableau
   };
 
   return (
-    <div className="game-list">
-      {games.map((game, index) => (
-        <Game key={index} game={game} deleteGame={deleteGame} />
-      ))}
-    </div>
+    <>
+      <button
+        onClick={() => {
+          setBestGames(!bestGames);
+        }}
+      >
+        {bestGames ? "All games" : "Best Games"}
+      </button>
+      <div className="game-list">
+        {games.map((game, index) => (
+          <Game key={index} game={game} deleteGame={deleteGame} />
+        ))}
+      </div>
+    </>
   );
 };
 
