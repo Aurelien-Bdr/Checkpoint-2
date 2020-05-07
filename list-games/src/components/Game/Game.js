a82fffa6a32d2504a6f22f02cf9ab05e657ef34c
Aurelien <aurelien.badre.dev@gmail.com>
2020-05-07 09:36:34 +0200

    cleanup and create GamList and Game component

---
+++ list-games/src/components/Game/Game.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Game.css";
+
+const Game = () => {
+  return <div>test</div>;
+};
+
+export default Game;
---
+++ list-games/src/components/GameList/GameList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./GameList.css";
+
+const GameList = () => {
+  return <div>test</div>;
+};
+
+export default GameList;
