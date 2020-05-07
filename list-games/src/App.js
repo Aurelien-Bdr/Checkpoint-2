8330fb84215aeeb268b5727d517a8b844109e4b3
Aurelien <aurelien.badre.dev@gmail.com>
2020-05-07 09:42:18 +0200

    Install Axios and call API

--- list-games/src/App.js
+++ list-games/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import GameList from "./components/GameList/GameList";
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World</p>
+      <GameList />
     </div>
   );
 }
--- list-games/src/components/GameList/GameList.js
+++ list-games/src/components/GameList/GameList.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./GameList.css";
 
 const GameList = () => {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(" https://wild-games.herokuapp.com/api/v1")
+      .then((res) => setGames(res.data));
+  }, [games]);
+
   return <div>test</div>;
 };
 
